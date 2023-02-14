7fd99e90e4dccdf4b687edd065afc42be6d30e65
Андрей Тараскин <a.taraskin@rlt.ru>
2023-02-14 14:00:46 +0300

    no reboot witchout sd-card

--- ports/satellit-a10/cube/Core/Src/main.c
+++ ports/satellit-a10/cube/Core/Src/main.c
@@ -146,7 +146,7 @@ void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-  HAL_NVIC_SystemReset();
+
   /* USER CODE END Error_Handler_Debug */
 }
 
@@ -163,7 +163,7 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  HAL_NVIC_SystemReset();
+
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
